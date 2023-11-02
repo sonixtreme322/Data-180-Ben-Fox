16e4e0a664344ff93479372ac8de4c22ec160cb6
Benjamin Fox <sonixtreme322@gmail.com>
2023-11-02 15:40:37 -0400

    Classwork

--- apply_function in R.R
+++ apply_function in R.R
@@ -3,8 +3,9 @@
 Test<- matrix(sample(1:20,12, replace = T), nrow=3, byrow = T)
 Test # This matrix will be different every time you run the line 3. 
 
-
+#apply(dataset/matrix, margin(rows/columns or both), mean/sd/sum)
 apply(Test,1,mean)
+#1 means rows, 2 means columns. 0 means both.
 
 apply(Test,2,mean)
 
@@ -14,7 +15,7 @@ apply(Test,2,sum)
 
 apply(Test,1,sum)
 
-apply(Test,2,sort, decreasing = T)
+apply(Test,2,sort, decreasing = T) #This one specifically sorts each column of the matrix so that their values are decreasing
 
 v1<-c(2,6,4,1)
 v2<-c(21,34,55,11)
@@ -25,13 +26,24 @@ Data_M
 
 apply(Data_M,2,mean)
 Data_M_scaled<-scale(Data_M)
+#Gives us a z-score table, as well as its mean and standard deviation
 Data_M_scaled
-
 
 apply(Data_M,2,sd)
 
+#This set of applications is finding the range by finding the maximum and minimum of the dataset
 rng<-apply(Data_M,2,max)-apply(Data_M,2,min)
 rng
 
+#Scales the data and returns a vector of the column ranges and a table dividing each value in the data frame by the range of values of the column that it's in.
 scale(Data_M, center = F, scale=rng)
+
+
+#New data library to do with planets, funnnn
+#install.packages("planets")
+library(planets)
+
+head(planets)
+
+library(ggplot2)
 
